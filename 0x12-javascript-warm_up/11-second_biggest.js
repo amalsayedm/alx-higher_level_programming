654a6c33115c713dceddf689590850eb3897b14e
amalsayedm <amal.sayedm2@gmail.com>
2023-09-14 21:15:38 +0200

    amal

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,12 @@ const size = process.argv.length;
 if (size <= 3) {
   console.log('0');
 } else {
-  const array = [];
+  let array = [];
   let i = 0;
   for (let x = 2; x < size; x++) {
     array[i] = parseInt(process.argv[x]);
     i++;
   }
-  array.sort();
-  console.log(array[array.length - 2]);
+ array = array.sort(function (a, b) { return a - b; });
+  console.log(array[array.length-2]);
 }
