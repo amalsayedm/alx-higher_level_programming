3304d79e932e2e465ec2d31785955df464bc1d0f
amalsayedm <amal.sayedm2@gmail.com>
2023-09-14 20:16:08 +0200

    amal

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-let x = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 if (x) {
-    for(let i = x;i > 0; i--){
-        let row ='';
-        for(let y = x; y > 0; y--){
-            row+='X'
-        }
-        console.log(row);
+  for (let i = x; i > 0; i--) {
+    let row = '';
+    for (let y = x; y > 0; y--) {
+      row += 'X';
     }
-    
-  } else {
-    console.log('Missing size');
+    console.log(row);
   }
-  
+} else {
+  console.log('Missing size');
+}
