185f2f73c728a700352af3c0962a0297bf1d7b2a
amalsayedm <amal.sayedm2@gmail.com>
2023-09-16 23:07:18 +0200

    amal

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,15 +2,13 @@
 """ Select states with names matching arguments """
 
 
-from sys import argv
-import MySQLdb
-
 if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
     db_user = argv[1]
     db_passwd = argv[2]
     db_name = argv[3]
-    search = '{}'.format(argv[4])
 
     database = MySQLdb.connect(host='localhost',
                                port=3306,
@@ -20,9 +18,10 @@ if __name__ == '__main__':
 
     cursor = database.cursor()
 
-    cursor.execute('SELECT id, name FROM states\
-                   WHERE name = %s\
-                   ORDER BY states.id ASC;', (search,))
+    cursor.execute('SELECT cities.id, cities.name, states.name FROM cities\
+                   JOIN states\
+                   ON cities.state_id = states.id\
+                   ORDER BY cities.id ASC')
 
     for row in cursor.fetchall():
         print(row)
