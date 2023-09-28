2ade40519641c3bc25c5079fd4b71e5e3aaaa8e4
amalsayedm <amal.sayedm2@gmail.com>
2023-09-29 01:01:52 +0200

    amal

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,8 +11,10 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     if list_of_integers[0] > list_of_integers[1]:
         return list_of_integers[0]
+    if list_of_integers[length - 1] > list_of_integers[length - 2]:
+        return list_of_integers[length - 1]
     i = 1
-    for i in range(length-2):
+    for i in range(length-3):
         if ((list_of_integers[i] > list_of_integers[i-1]) and
                 (list_of_integers[i] > list_of_integers[i+1])):
             return list_of_integers[i]
