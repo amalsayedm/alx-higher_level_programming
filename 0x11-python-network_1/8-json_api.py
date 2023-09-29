ed4d73f90bd479db67afec498dc29decb1b4489a
amalsayedm <amal.sayedm2@gmail.com>
2023-09-30 00:39:08 +0200

    amal

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request with agiven data"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    data = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
