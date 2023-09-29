d340fd39d6946886c844357eb36f582d60c276de
amalsayedm <amal.sayedm2@gmail.com>
2023-09-29 16:44:58 +0200

    amal

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Fetch url response"""
-
+"""Fetches url resource."""
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    content = response.read()
-print('Body response:')
-print('\t- type: {}'.format(type(content)))
-print('\t- content: {}'.format(content))
-print('\t- utf8 content: {}'.format(str(content)[2:-1]))
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
